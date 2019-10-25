7efff30d053744f09b9ef6ac053d3b260088087d
huangzijie <huangzijie@zenchn.com>
2019-10-25 17:40:26 +0800

    vue.js 学习

--- app/src/main/assets/myScript.js
+++ app/src/main/assets/myScript.js
@@ -1,4 +1,10 @@
 //外部js文件
 function tellMe(){
+    /*
+    我是多行注释
+    哈哈哈哈
+    斤斤计较
+    */
     document.getElementById("p2").innerHTML="我是外部js传回来的话";
+    document.getElementById("p3").innerHTML="黑猫警长在哪里？我是蓝猫啊";
 }
